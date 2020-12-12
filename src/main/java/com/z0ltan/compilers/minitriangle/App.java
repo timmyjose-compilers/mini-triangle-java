04467f2de03d8c3526587bcbdff9da7c9481dd6e
Timmy Jose <zoltan.jose@gmail.com>
2020-12-12 17:44:29 +0530

    initial project setup

---
+++ src/main/java/com/z0ltan/compilers/minitriangle/App.java
@@ -0,0 +1,13 @@
+package com.z0ltan.compilers.minitriangle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
